9d2321278bc8a42b4cb51196c20b8073d02e9e8c
Brandon Munda <mundabrandon@outlook.com>
2024-10-04 16:13:58 +0300

    chore: Add route for sending notifications

--- zoho/webhooks/urls.py
+++ zoho/webhooks/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("webhook/", views.zoho_webhook, name="zoho_webhook"),
-    path("forms/", views.list_zoho_forms, name="list_zoho_forms"),
-    path("table/data/", views.display_data, name="display_table_data"),
+    path("notifications/send/", views.send_notifications, name="send-notifications"),
 ]
