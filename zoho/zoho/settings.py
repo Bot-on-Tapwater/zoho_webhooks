25ed552b54cce79e5558732d4360b9bdd0c23d8b
Brandon Munda <mundabrandon@outlook.com>
2024-10-04 16:11:53 +0300

    chore: Add smtp configuration variables

--- zoho/zoho/settings.py
+++ zoho/zoho/settings.py
@@ -137,3 +137,12 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # Replace with your email
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")   # Replace with your email password
