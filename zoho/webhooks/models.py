d31c07e53da39deb1fade8300061dbfc757eb9f7
Brandon Munda <mundabrandon@outlook.com>
2024-10-04 16:12:39 +0300

    feat: Add email and whatsapp notifications model

--- zoho/webhooks/models.py
+++ zoho/webhooks/models.py
@@ -26,3 +26,12 @@ class ZohoFormData(models.Model):
 
     def __str__(self):
         return f"Zoho Form Data {self.id}"
+
+class EmailWhatsappNotifications(models.Model):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    registration = models.BooleanField(default=True)
+    onboarding = models.BooleanField(default=False)
+    activation = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # Set on creation
+    updated_at = models.DateTimeField(auto_now=True)      # Updated on each save
